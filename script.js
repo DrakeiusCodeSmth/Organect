b5f633e19ca15511d760743f10c6c0ac99f93f8c
DrakeiusCodeSmth <darius.anggada05@gmail.com>
2024-11-29 15:49:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,30 +11,44 @@ function showContent(section) {
         contentArea.innerHTML = "<h2>Welcome to the Main Menu</h2><p>This is the main menu. Click any of the buttons above to navigate.</p>";
     } else if (section === 'faq') {
         contentArea.innerHTML = `
-            <h2>FAQ</h2>
+            <h2>Contact Us</h2>
+            <p>If you have any questions, please fill in the form below:</p>
             <form class="faq-form" action="mailto:darius.anggada05@gmail.com" method="POST" enctype="multipart/form-data">
                 <input type="text" name="name" placeholder="Your Name" required>
                 <input type="email" name="email" placeholder="Your Email" required>
-                <textarea name="message" placeholder="Your Message" rows="4" required></textarea>
-                <button type="submit">Submit</button>
-            </form>`;
+                <textarea name="message" rows="5" placeholder="Your Message" required></textarea>
+                <button type="submit">Send Message</button>
+            </form>
+        `;
     } else if (section === 'devs') {
         contentArea.innerHTML = `
-            <h2>Meet The Devs</h2>
-            <div class="profile-container">
-                <div class="profile-card">
-                    <img src="images/dev1.jpg" alt="Dev 1">
-                    <div class="profile-card-info">Dev 1 Info</div>
+            <h2>Meet the Developers</h2>
+            <div class="devs-cards">
+                <div class="dev-card">
+                    <img src="images/dev1.jpg" alt="Developer 1">
+                    <div class="dev-info">Developer 1</div>
                 </div>
-                <div class="profile-card">
-                    <img src="images/dev2.jpg" alt="Dev 2">
-                    <div class="profile-card-info">Dev 2 Info</div>
+                <div class="dev-card">
+                    <img src="images/dev2.jpg" alt="Developer 2">
+                    <div class="dev-info">Developer 2</div>
                 </div>
-                <div class="profile-card">
-                    <img src="images/dev3.jpg" alt="Dev 3">
-                    <div class="profile-card-info">Dev 3 Info</div>
+                <div class="dev-card">
+                    <img src="images/dev3.jpg" alt="Developer 3">
+                    <div class="dev-info">Developer 3</div>
                 </div>
-                <div class="profile-card">
-                    <img src="images/dev4.jpg" alt="Dev 4">
-                    <div class="profile-card-info">Dev 4 Info</div>
-                </
+                <div class="dev-card">
+                    <img src="images/dev4.jpg" alt="Developer 4">
+                    <div class="dev-info">Developer 4</div>
+                </div>
+                <div class="dev-card">
+                    <img src="images/dev5.jpg" alt="Developer 5">
+                    <div class="dev-info">Developer 5</div>
+                </div>
+                <div class="dev-card">
+                    <img src="images/dev6.jpg" alt="Developer 6">
+                    <div class="dev-info">Developer 6</div>
+                </div>
+            </div>
+        `;
+    }
+}
